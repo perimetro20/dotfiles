6642eac1d04ee323220f93e6abcb127719ad0787
Aldo Andreé Toledo Ulloa <perimetro20@Aldos-MacBook-Pro.local>
2023-03-11 13:34:47 -0600

    Create bootstrap setup for emacs and karabiner

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-# Install homebrew
+DOTFILES_PATH=$(pwd)
 
-which brew
+# Setup Emacs
+echo "Setting up Emacs\n"
+EMACS_CONFIG_PATH=$HOME/.emacs.d
+mkdir -p $EMACS_CONFIG_PATH
+ln -s $DOTFILES_PATH/settings.org $EMACS_CONFIG_PATH
+ln -s $DOTFILES_PATH/init.el $EMACS_CONFIG_PATH
+
+# Setup Karabiner
+echo "Setting up Karabiner\n"
+KARABINER_CONFIG_PATH=$HOME/.config/karabiner
+mkdir -p $KARABINER_CONFIG_PATH
+ln -s $DOTFILES_PATH/karabiner.json $KARABINER_CONFIG_PATH
