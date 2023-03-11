4059da5750a99a4b4d02877c692f6919eec553b7
Aldo Andree Toledo Ulloa <aldotol@amazon.com>
2023-03-11 11:12:16 -0600

    Add emacs and karabiner files

---
+++ init.el
@@ -0,0 +1,17 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+(require 'org)
+(org-babel-load-file
+  (expand-file-name "settings.org"
+     user-emacs-directory))
+
