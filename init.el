63235bebdf04a61f245d9b4df648eb17e1e5ca97
Aldo Andreé Toledo Ulloa <aandreetoledo@gmail.com>
2024-01-15 19:03:33 -0600

    Small modifications to emacs init file

--- init.el
+++ init.el
@@ -1,8 +1,14 @@
+;;; package --- My init file
+;;; Commentary:
+;;; This is my personal init file, it bootstrap the basics to have a full file
+;;; org file.
+
+;;; Code:
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-;; Bootstrap `use-package'
+;; Bootstrap use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
@@ -14,3 +20,13 @@
 (org-babel-load-file
   (expand-file-name "settings.org"
      user-emacs-directory))
+
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-keybinding nil)
+  :config
+  (require 'evil-org-agenda))
+
+(provide 'init)
+;;; init.el ends here
